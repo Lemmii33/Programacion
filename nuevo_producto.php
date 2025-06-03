94248de7bde9e211131dc47525b6467b0888ab3b
Lemmii33 <lemmii@gmail.com>
2025-06-03 12:53:47 +0200

    correccion de errores

--- nuevo_producto.php
+++ nuevo_producto.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php
 include 'conexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
