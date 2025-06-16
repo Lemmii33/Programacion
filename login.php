a756ff4049a65c3d8cd5db2c8258ae8ce33b94ff
Lemmii33 <lemmii@gmail.com>
2025-06-16 11:07:03 +0200

    conexion delante

--- login.php
+++ login.php
@@ -1,10 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
-<?php
 session_start();
 require_once 'conexion.php';
 
@@ -32,7 +26,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
