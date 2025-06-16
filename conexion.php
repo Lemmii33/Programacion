599fd02a617b329037cb8638f58143e8b7fadc9d
Lemmii33 <lemmii@gmail.com>
2025-06-16 13:34:33 +0200

    ccd

--- conexion.php
+++ conexion.php
@@ -8,7 +8,7 @@ function conectar() {
     $conexion = new mysqli($host, $user, $pass, $db);
 
     if ($conexion->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
+        die("Conexión fallida: " . $conexion->connect_error);
     }
 
     return $conexion;
